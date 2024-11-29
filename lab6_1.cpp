3637b91f99aee876b96e3643354a9e40c298394d
kasidin <kasidinkhamfoo@gmail.com>
2024-11-29 16:21:30 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    int num;
+    int i = 0;
+    int Even = 0;
+    int Odd = 0;
+    while (i < 1){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if(num == 0){
+            i++;
+        }
+        else if (num %2==0){
+            Even++;
+        }
+        else if (num %2!=0){
+            Odd++;
+        }
+     }
+    
+cout << "#Even numbers = " << Even <<"\n";
+
+cout << "#Odd numbers = " << Odd;
+return 0;
 }
